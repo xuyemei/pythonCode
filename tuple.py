6e7c5fe70ff7c72a6a53314ee6359b7e65a66ccb
1454346617 <1454346617@qq.com>
2019-12-11 15:19:09 +0800

    '列表跟元祖'

---
+++ tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+l1 = ['aaa','bbb','ccc','ddd']
+l2 = ['aaa2','bbb2','ccc2','ddd2']
+
+print(l1 +l2)#['aaa', 'bbb', 'ccc', 'ddd', 'aaa2', 'bbb2', 'ccc2', 'ddd2']
+
+print(l1*2)#['aaa', 'bbb', 'ccc', 'ddd', 'aaa', 'bbb', 'ccc', 'ddd']
